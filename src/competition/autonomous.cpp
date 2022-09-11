f529bef0d5d25978a3a9ba594c1b3653d6c2b9ad
Christopher Nokes <crn2267@rit.edu>
2022-09-11 13:28:26 -0400

    AAAAAAAAAAAAAAAAAAA

--- src/competition/autonomous.cpp
+++ src/competition/autonomous.cpp
@@ -7,8 +7,8 @@
 
 void autonomous()
 {
-  drive_system.drive_tank(.25, .25);
-  wait(4, seconds);
+  // drive_system.drive_tank(.25, .25);
+  // wait(4, seconds);
   for(int iii = 0; iii < 4; iii++) {
     drive_system.drive_forward(12, .25, 1, fwd);
     drive_system.turn_degrees(90, .25);
