3f21fd855011c532878b3a426c2b0a7fa831b3ef
MJE10 <63426573+MJE10@users.noreply.github.com>
2023-10-19 19:16:31 -0400

    pure pursuit test

--- src/competition/autonomous.cpp
+++ src/competition/autonomous.cpp
@@ -10,18 +10,29 @@ void autonomous()
     printf("starting!\n");
     fflush(stdout);
 
+    std::vector<point_t> path = {
+        {0, 0},
+        {15.7, 68},
+        {10.4, 86.5},
+        {-3.5, 82.3},
+        {-15, 77.3},
+        {-22.6, 98.6},
+        {9.2, 122}
+    };
+
     CommandController command_controller {
-        drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
-        drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
-        drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 15),
-        drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
-        drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 0),
+        drive_system.PurePursuitCmd(motion_controller, path, directionType::fwd, 1, 0)
+        // drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
+        // drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
+        // drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 15),
+        // drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 30),
+        // drive_system.DriveForwardCmd(motion_controller, 18.0, directionType::fwd, 1, 0),
         
-        drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
-        drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
-        drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -15),
-        drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
-        drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, 0),
+        // drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
+        // drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
+        // drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -15),
+        // drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, -30),
+        // drive_system.DriveForwardCmd(motion_controller, -18.0, directionType::rev, 1, 0),
     };
     command_controller.run();
 }
